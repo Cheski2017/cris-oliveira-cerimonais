c187937c566bfe39cd00cb7a42c3eef445624ed4
Cheski2017 <lucas_cheski@hotmail.com>
2026-07-03 22:43:35 -0300

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  const clientId = '0v23liOfBjrF2IEePqOU';
+  const clientId = 'Ov23liOfBjrF2IEePqOU';
   const scope = 'repo,user';
   return Response.redirect(
     `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}`,
